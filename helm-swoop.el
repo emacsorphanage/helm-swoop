a4110a82fafb3bc098eb4923692601cdef9187d9
FukuyamaShingo <houseshou@FukuyamaShingo-no-MacBook-Pro.local>
2014-12-18 14:03:07 +0900

    Fix transition canceling from helm-swoop to helm-multi-swoop-all

--- helm-swoop.el
+++ helm-swoop.el
@@ -1180,8 +1180,8 @@ Last selected buffers will be applied to helm-multi-swoop.
   (helm-swoop--restore)
   (delete-overlay helm-swoop-line-overlay)
   (setq helm-multi-swoop-all-from-helm-swoop-last-point helm-swoop-last-point)
-  (helm-quit-and-execute-action
-   (lambda (ignored) (helm-multi-swoop-all helm-pattern))))
+  (run-with-timer 0 nil (lambda () (helm-multi-swoop-all helm-pattern)))
+  (helm-exit-minibuffer))
 
 (defadvice helm-resume (around helm-multi-swoop-resume activate)
   "Resume if the last used helm buffer is *Helm Swoop*"
