d834b05538dd3381c68f1260d39d3a7eb6a8f7f7
FukuyamaShingo <houseshou@FukuyamaShingos-MacBook-Pro.local>
2015-04-25 22:35:17 +0900

    v1.5.1 for Melpa Stable

--- helm-swoop.el
+++ helm-swoop.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2013 by Shingo Fukuyama
 
-;; Version: 1.5.0
+;; Version: 1.5.1
 ;; Author: Shingo Fukuyama - http://fukuyama.co
 ;; URL: https://github.com/ShingoFukuyama/helm-swoop
 ;; Created: Oct 24 2013
