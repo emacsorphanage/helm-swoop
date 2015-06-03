76545923c2f970738e33d691c3a1b614cda19f7a
Shingo Fukuyama <houseshou@Shingos-MacBook-Pro.local>
2015-06-04 01:38:11 +0900

    ver 1.6.0

--- helm-swoop.el
+++ helm-swoop.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2013 by Shingo Fukuyama
 
-;; Version: 1.5.1
+;; Version: 1.6.0
 ;; Author: Shingo Fukuyama - http://fukuyama.co
 ;; URL: https://github.com/ShingoFukuyama/helm-swoop
 ;; Created: Oct 24 2013
