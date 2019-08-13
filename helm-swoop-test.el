6c295888c6ccb718af569df2b851e800a8bfa5be
conao3 <conao3@gmail.com>
2019-08-13 19:17:28 +0900

    add empty testcase file

---
+++ helm-swoop-test.el
@@ -0,0 +1,24 @@
+;;; helm-swoop-test.el --- Test file for helm-swoop -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Emacsorphanage
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Maintainer: Emacsorphanage
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
+;;; Commentary:
+
+(load "cort-test")
+(require 'helm-swoop)
+
+(provide 'helm-swoop-test)
+;;; helm-swoop-test.el ends here
