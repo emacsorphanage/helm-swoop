6372836e4ebdaa9f0ed2adb6d0a4388ac140ea6f
conao3 <conao3@gmail.com>
2019-08-13 21:50:36 +0900

    require helm v3.2

--- helm-swoop.el
+++ helm-swoop.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/ShingoFukuyama/helm-swoop
 ;; Created: Oct 24 2013
 ;; Keywords: helm swoop inner buffer search
-;; Package-Requires: ((helm "1.0") (emacs "24.4"))
+;; Package-Requires: ((helm "3.2") (emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
